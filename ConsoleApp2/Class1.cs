741b04602208da69d19fb7ec32db48a66f741822
panda1331 <evelinamorova27@gmail.com>
2026-01-31 18:19:48 +0300

    add new class Class1 with method SayWord add new item class1 and call method SayWord

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2;
+
+public class Class1
+{
+    public void SayWord(string word) => Console.WriteLine(word);
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("kjg;sig");
+Class1 class1 = new Class1();
+class1.SayWord("you");
